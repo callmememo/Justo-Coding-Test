5a904f50f8637b88a86283925418345c9a63e6be
Guillermo Rodriguez <grdz2404@gmail.com>
2021-03-26 15:29:56 -0600

    Initial Commit

---
+++ Coding Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coding Test
+//
+//  Created by Memo on 3/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
